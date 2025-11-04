1e0677ad4a5f6e6ebd97a5d34c9aeadbc87243f5
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2025-11-04 00:36:36 -0500

    Clean up unused vars

--- src/particles/particles.hpp
+++ src/particles/particles.hpp
@@ -55,9 +55,6 @@ class Particles {
   ParticleType particle_type;
   int nprtcl_thispack;             // number of particles this MeshBlockPack
   int nrdata, nidata;
-//  DvceArray1D<int>  prtcl_gid;     // GID of MeshBlock containing each par
-//  DvceArray2D<Real> prtcl_pos;     // positions
-//  DvceArray2D<Real> prtcl_vel;     // velocities
   DvceArray2D<Real> prtcl_rdata;   // real number properties each particle (x,v,etc.)
   DvceArray2D<int>  prtcl_idata;   // integer properties each particle (gid, tag, etc.)
   Real dtnew;
